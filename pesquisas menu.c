85de47a601d9868437b57c17b0db33915140ff23
cosmosgc <cosmoskitsune@hotmail.com>
2023-04-27 21:16:58 -0300

    Update pesquisas menu.c

--- pesquisas menu.c
+++ pesquisas menu.c
@@ -1,48 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "pesquisas.c"
 
 int main() {
-    int opcao, chave, resultado;
-    int v[] = {1, 2, 3, 4, 5};
+    int opcao, chave, resultado, n;
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &n);
 
-    printf("Escolha uma opcao:\n");
-    printf("1. Pesquisa Sequencial\n");
-    printf("2. Pesquisa Sequencial com Sentinela\n");
-    printf("3. Pesquisa Binaria\n");
-    printf("4. Pesquisa Binaria Recursiva\n");
-    printf("5. Pesquisa por Interpolacao\n");
-    printf("Opcao: ");
-    scanf("%d", &opcao);
+    int v[n];
 
-    printf("Digite a chave a ser procurada: ");
-    scanf("%d", &chave);
+    // Inicializa a semente do gerador de números aleatórios
+    srand(time(NULL));
 
-    switch (opcao) {
-        case 1:
-            resultado = pesquisaSequencial(chave, v, 5);
-            break;
-        case 2:
-            resultado = pesquisaSequencialSentinela(chave, v, 5);
-            break;
-        case 3:
-            resultado = pesquisaBinaria(chave, v, 5);
-            break;
-        case 4:
-            resultado = pesquisaBinariaRecursiva(chave, v, 0, 4);
-            break;
-        case 5:
-            resultado = pesquisaInterpolacao(chave, v, 5);
-            break;
-        default:
-            printf("Opcao invalida!\n");
-            return 1;
+    // Preenche o vetor com valores aleatórios entre 1 e 100
+    for (int i = 0; i < n; i++) {
+        v[i] = rand() % 100 + 1;
     }
+    while(1){
+        printf("Escolha uma opcao:\n");
+        printf("1. Pesquisa Sequencial\n");
+        printf("2. Pesquisa Sequencial com Sentinela\n");
+        printf("3. Pesquisa Binaria\n");
+        printf("4. Pesquisa Binaria Recursiva\n");
+        printf("5. Pesquisa por Interpolacao\n");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
 
-    if (resultado == -1) {
-        printf("Chave nao encontrada.\n");
-    } else {
-        printf("Chave encontrada no indice %d.\n", resultado);
+        printf("Digite a chave a ser procurada: ");
+        scanf("%d", &chave);
+
+        switch (opcao) {
+            case 1:
+                resultado = pesquisaSequencial(chave, v, n);
+                break;
+            case 2:
+                resultado = pesquisaSequencialSentinela(chave, v, n);
+                break;
+            case 3:
+                resultado = pesquisaBinaria(chave, v, n);
+                break;
+            case 4:
+                resultado = pesquisaBinariaRecursiva(chave, v, 0, n-1);
+                break;
+            case 5:
+                resultado = pesquisaInterpolacao(chave, v, n);
+                break;
+            default:
+                printf("Opcao invalida!\n");
+                //return 1;
+        }
+
+        if (resultado == -1) {
+            printf("Chave nao encontrada.\n");
+        } else {
+            printf("Chave encontrada no indice %d.\n", resultado);
+        }
+
+        //return 0;
     }
-
-    return 0;
+    
 }
