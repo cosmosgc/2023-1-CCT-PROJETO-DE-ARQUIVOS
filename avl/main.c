cf25451bfcbe199b2ac98e0a1ff7e0e94b00a192
cosmosgc <cosmoskitsune@hotmail.com>
2023-05-18 20:00:54 -0300

    2 exercicios do pdf

---
+++ avl/main.c
@@ -0,0 +1,41 @@
+#include "avl.c"
+int main() {
+    AVLArvore* a = AVLcriar();
+
+    for (int i = 1; i <= 7; i++) {
+        AVLadicionar(a,i);
+    }
+
+    printf("\n");
+    printf("Contador: %i\n", AVLcontador);
+    AVLcontador=0;
+
+    AVLArvore* b = AVLcriar();
+
+    AVLadicionar(b,4);
+    AVLadicionar(b,2);
+    AVLadicionar(b,8);
+    AVLadicionar(b,1);
+    AVLadicionar(b,3);
+    AVLadicionar(b,6);
+    AVLadicionar(b,9);
+    AVLadicionar(b,5);
+    AVLadicionar(b,7);
+
+    printf("Contador: %i\n", AVLcontador);
+    printf("\n");
+    //AVLpercorrerProfundidadeInOrder(b);
+    
+    AVLNo* avlDeletar = AVLlocalizar(b->raiz, 6);
+    AVLremover(b, avlDeletar);
+
+    int localizado = 0;
+    AVLNo* avlLocalizado = AVLlocalizar(b->raiz, 6);
+    if(avlLocalizado!=NULL){
+        localizado = avlLocalizado->valor;
+        printf("resultado de procurar o nó 6: %i\n", localizado);
+    }else{
+        printf("resultado de procurar o nó 6: Nó não encontrado\n");
+    }
+    AVLcontador=0;
+}
